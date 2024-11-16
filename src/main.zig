4027e806ed7c0b63bb1675415d05960fa083e8e8
Kaio Delphino <dash.zet@gmail.com>
2024-11-16 13:39:38 -0300

    initial commit

---
+++ src/main.zig
@@ -0,0 +1,22 @@
+const std = @import("std");
+const rl = @cImport({
+    @cInclude("raylib.h");
+});
+
+pub fn main() !void {
+    rl.SetConfigFlags(rl.FLAG_VSYNC_HINT);
+    rl.SetTargetFPS(60);
+    rl.InitWindow(1080, 720, "isometric grid");
+    defer rl.CloseWindow();
+
+    while (!rl.WindowShouldClose()) {
+        try update();
+        try draw();
+    }
+}
+
+fn update() !void {
+    if (rl.IsKeyPressed(rl.KEY_ESCAPE)) rl.CloseWindow();
+}
+
+fn draw() !void {}
