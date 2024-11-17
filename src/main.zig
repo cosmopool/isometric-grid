896ddbd55d8a752ad8feef9d52fd6b64f97e0b9c
Kaio Delphino <dash.zet@gmail.com>
2024-11-16 23:52:18 -0300

    chore: improve readability

--- src/main.zig
+++ src/main.zig
@@ -13,18 +13,18 @@ const rows = 10;
 const columns = 10;
 const numberOfTiles = rows * columns;
 const tileSize = 40;
-var originalGrid: [numberOfTiles * 2]f32 = undefined;
+var symmetricalGrid: [numberOfTiles * 2]f32 = undefined;
 var grid: [numberOfTiles * 2]f32 = undefined;
 
-var originalVerticalGridLinesStart: [numberOfTiles * 2]f32 = undefined;
-var originalVerticalGridLinesEnd: [numberOfTiles * 2]f32 = undefined;
-var verticalGridLinesStart: [numberOfTiles * 2]f32 = undefined;
-var verticalGridLinesEnd: [numberOfTiles * 2]f32 = undefined;
+var symmetricalVerticalLinesStart: [numberOfTiles * 2]f32 = undefined;
+var symmetricalVerticalLinesEnd: [numberOfTiles * 2]f32 = undefined;
+var verticalLinesStart: [numberOfTiles * 2]f32 = undefined;
+var verticalLinesEnd: [numberOfTiles * 2]f32 = undefined;
 
-var originalHorizontalGridLinesStart: [numberOfTiles * 2]f32 = undefined;
-var originalHorizontalGridLinesEnd: [numberOfTiles * 2]f32 = undefined;
-var horizontalGridLinesStart: [numberOfTiles * 2]f32 = undefined;
-var horizontalGridLinesEnd: [numberOfTiles * 2]f32 = undefined;
+var symmetricalHorizontalLinesStart: [numberOfTiles * 2]f32 = undefined;
+var symmetricalHorizontalLinesEnd: [numberOfTiles * 2]f32 = undefined;
+var horizontalLinesStart: [numberOfTiles * 2]f32 = undefined;
+var horizontalLinesEnd: [numberOfTiles * 2]f32 = undefined;
 
 // grid visualization properties
 var offsetX: f32 = 0;
@@ -37,7 +37,8 @@ pub fn main() !void {
     rl.InitWindow(screenWidth, screenHeight, "isometric grid");
     defer rl.CloseWindow();
 
-    generateOriginalGrid();
+    // setup initial grid
+    generateSymmetricalGrid();
     transformGrid(1, tilt, -1, tilt);
 
     while (!rl.WindowShouldClose()) {
@@ -72,17 +73,17 @@ fn draw() !void {
 
         rl.DrawPixel(@intFromFloat(grid[x] + offsetX), @intFromFloat(grid[y] + offsetY), rl.YELLOW);
         rl.DrawLine(
-            @intFromFloat(verticalGridLinesStart[x] + offsetX),
-            @intFromFloat(verticalGridLinesStart[y] + offsetY),
-            @intFromFloat(verticalGridLinesEnd[x] + offsetX),
-            @intFromFloat(verticalGridLinesEnd[y] + offsetY),
+            @intFromFloat(verticalLinesStart[x] + offsetX),
+            @intFromFloat(verticalLinesStart[y] + offsetY),
+            @intFromFloat(verticalLinesEnd[x] + offsetX),
+            @intFromFloat(verticalLinesEnd[y] + offsetY),
             rl.BLUE,
         );
         rl.DrawLine(
-            @intFromFloat(horizontalGridLinesStart[x] + offsetX),
-            @intFromFloat(horizontalGridLinesStart[y] + offsetY),
-            @intFromFloat(horizontalGridLinesEnd[x] + offsetX),
-            @intFromFloat(horizontalGridLinesEnd[y] + offsetY),
+            @intFromFloat(horizontalLinesStart[x] + offsetX),
+            @intFromFloat(horizontalLinesStart[y] + offsetY),
+            @intFromFloat(horizontalLinesEnd[x] + offsetX),
+            @intFromFloat(horizontalLinesEnd[y] + offsetY),
             rl.BLUE,
         );
     }
@@ -90,40 +91,54 @@ fn draw() !void {
     rl.DrawFPS(rl.GetScreenWidth() - 95, 10);
 }
 
-inline fn generateOriginalGrid() void {
+/// The grid lines is separated in 4 arrays that store two-dimensional vector points
+/// - vertical lines start
+/// - vertical lines end
+/// - horizontal lines start
+/// - horizontal lines end
+/// This is because we can use just one loop of vector size 2
+/// to operate in grid tiles and grid lines at the same time.
+/// The alternative is one for-loop to iterate over the tiles
+/// and other to go over lines (maybe this is better/cleaner?).
+inline fn generateSymmetricalGrid() void {
     const halfTile = tileSize / 2;
     comptime var index: usize = 0;
     inline for (0..rows) |row| {
         inline for (0..columns) |column| {
-            if (index >= originalGrid.len) break;
+            if (index >= symmetricalGrid.len) break;
 
             const x = index + 0;
             const y = index + 1;
             assert(x < grid.len);
             assert(y <= grid.len);
-
-            // grid tiles coordinates
-            originalGrid[x] = @floatFromInt(column * tileSize);
-            originalGrid[y] = @floatFromInt(row * tileSize);
-
-            // grid lines coordinates
-            originalVerticalGridLinesStart[x] = @floatFromInt(column * tileSize + halfTile);
-            originalVerticalGridLinesStart[y] = -halfTile;
-            originalVerticalGridLinesEnd[x] = originalGrid[x] + halfTile;
-            originalVerticalGridLinesEnd[y] = tileSize * rows - halfTile;
-
             index += 2;
 
-            if (column > 0) continue;
+            // grid tiles coordinates
+            symmetricalGrid[x] = @floatFromInt(column * tileSize);
+            symmetricalGrid[y] = @floatFromInt(row * tileSize);
 
-            originalHorizontalGridLinesStart[x] = -halfTile;
-            originalHorizontalGridLinesStart[y] = @floatFromInt(row * tileSize + halfTile);
-            originalHorizontalGridLinesEnd[x] = tileSize * columns - halfTile;
-            originalHorizontalGridLinesEnd[y] = originalGrid[y] + halfTile;
+            // vertical grid lines coordinates
+            symmetricalVerticalLinesStart[x] = @floatFromInt(column * tileSize + halfTile);
+            symmetricalVerticalLinesStart[y] = -halfTile;
+            symmetricalVerticalLinesEnd[x] = symmetricalGrid[x] + halfTile;
+            symmetricalVerticalLinesEnd[y] = tileSize * rows - halfTile;
+
+            if (column > 0) continue;
+            // vertical grid lines coordinates
+            symmetricalHorizontalLinesStart[x] = -halfTile;
+            symmetricalHorizontalLinesStart[y] = @floatFromInt(row * tileSize + halfTile);
+            symmetricalHorizontalLinesEnd[x] = tileSize * columns - halfTile;
+            symmetricalHorizontalLinesEnd[y] = symmetricalGrid[y] + halfTile;
         }
     }
 }
 
+/// will multiply the vector [[pointX, pointY]] by the matrix [[[ax, bx]], [[ay, by]]]
+/// --      --   --    --
+/// | pointX |   |ax, bx|
+/// | pointX | x |ay, by|
+/// --      --   --    --
+/// https://en.wikipedia.org/wiki/Matrix_multiplication
 fn transformPoints(pointX: f32, pointY: f32, ax: f32, ay: f32, bx: f32, by: f32) [2]f32 {
     const transformedX: f32 = pointX * ax + pointY * bx;
     const transformedY: f32 = pointX * ay + pointY * by;
@@ -140,30 +155,30 @@ fn transformGrid(ax: f32, ay: f32, bx: f32, by: f32) void {
         assert(x < grid.len);
         assert(y <= grid.len);
 
-        const transformedPoints: [2]f32 = transformPoints(originalGrid[x], originalGrid[y], ax, ay, bx, by);
+        const transformedPoints: [2]f32 = transformPoints(symmetricalGrid[x], symmetricalGrid[y], ax, ay, bx, by);
         grid[x] = transformedPoints[0];
         grid[y] = transformedPoints[1];
 
         // vertical grid lines
-        const verticalTransformedLinesPointsStart: [2]f32 = transformPoints(originalVerticalGridLinesStart[x], originalVerticalGridLinesStart[y], ax, ay, bx, by);
-        verticalGridLinesStart[x] = verticalTransformedLinesPointsStart[0];
-        verticalGridLinesStart[y] = verticalTransformedLinesPointsStart[1];
-        const verticalTransformedLinesPointsEnd: [2]f32 = transformPoints(originalVerticalGridLinesEnd[x], originalVerticalGridLinesEnd[y], ax, ay, bx, by);
-        verticalGridLinesEnd[x] = verticalTransformedLinesPointsEnd[0];
-        verticalGridLinesEnd[y] = verticalTransformedLinesPointsEnd[1];
+        const verticalTransformedLinesPointsStart: [2]f32 = transformPoints(symmetricalVerticalLinesStart[x], symmetricalVerticalLinesStart[y], ax, ay, bx, by);
+        verticalLinesStart[x] = verticalTransformedLinesPointsStart[0];
+        verticalLinesStart[y] = verticalTransformedLinesPointsStart[1];
+        const verticalTransformedLinesPointsEnd: [2]f32 = transformPoints(symmetricalVerticalLinesEnd[x], symmetricalVerticalLinesEnd[y], ax, ay, bx, by);
+        verticalLinesEnd[x] = verticalTransformedLinesPointsEnd[0];
+        verticalLinesEnd[y] = verticalTransformedLinesPointsEnd[1];
 
         // horizontal grid lines
-        const horizontalTransformedLinesPointsStart: [2]f32 = transformPoints(originalHorizontalGridLinesStart[x], originalHorizontalGridLinesStart[y], ax, ay, bx, by);
-        horizontalGridLinesStart[x] = horizontalTransformedLinesPointsStart[0];
-        horizontalGridLinesStart[y] = horizontalTransformedLinesPointsStart[1];
-        const horizontalTransformedLinesPointsEnd: [2]f32 = transformPoints(originalHorizontalGridLinesEnd[x], originalHorizontalGridLinesEnd[y], ax, ay, bx, by);
-        horizontalGridLinesEnd[x] = horizontalTransformedLinesPointsEnd[0];
-        horizontalGridLinesEnd[y] = horizontalTransformedLinesPointsEnd[1];
+        const horizontalTransformedLinesPointsStart: [2]f32 = transformPoints(symmetricalHorizontalLinesStart[x], symmetricalHorizontalLinesStart[y], ax, ay, bx, by);
+        horizontalLinesStart[x] = horizontalTransformedLinesPointsStart[0];
+        horizontalLinesStart[y] = horizontalTransformedLinesPointsStart[1];
+        const horizontalTransformedLinesPointsEnd: [2]f32 = transformPoints(symmetricalHorizontalLinesEnd[x], symmetricalHorizontalLinesEnd[y], ax, ay, bx, by);
+        horizontalLinesEnd[x] = horizontalTransformedLinesPointsEnd[0];
+        horizontalLinesEnd[y] = horizontalTransformedLinesPointsEnd[1];
     }
 }
 
-fn tiltBoard(by: f32) void {
-    if (tilt + by > 1 or tilt + by < 0) return;
-    tilt += by;
+fn tiltBoard(amountToTilt: f32) void {
+    if (tilt + amountToTilt > 1 or tilt + amountToTilt < 0) return;
+    tilt += amountToTilt;
     transformGrid(1, tilt, -1, tilt);
 }
